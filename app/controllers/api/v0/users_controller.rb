58f03c037461475e911da2eb5b568d20abb829de
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-14 14:23:39 -0600

    Add deleting a user functionality and error handling (if user not found w/ params provided)

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,6 +1,9 @@
 class Api::V0::UsersController < ApplicationController
+	rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
 	def create
 		user = User.new(user_params)
+
 		if user.save
 			render json: UserSerializer.new(user), status: :ok
 		else
@@ -10,12 +13,19 @@ class Api::V0::UsersController < ApplicationController
 
 	def destroy
 		user = User.find_by(user_params)
-		User.destroy(user)
-
-		
+		if user
+			user.destroy
+			render json: { message: "User #{user.email}, was successfully deleted" }, status: :ok
+		else
+			handle_record_not_found
+		end
 	end
 
 	private
+
+	def handle_record_not_found
+		render json: { error: 'User not found' }, status: :not_found
+	end
 
 	def user_params
 		params.require(:user).permit(:id, :email)
