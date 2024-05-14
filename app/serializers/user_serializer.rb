ac533baab8310bbbff001f9699b81c23129cb2b4
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-14 13:12:13 -0600

    Remove all api_key refs and methods

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer
 	include JSONAPI::Serializer
 
-	attributes :email, :api_key
+	attributes :email
 end
--- spec/requests/a_user_can_be_created_spec.rb
+++ spec/requests/a_user_can_be_created_spec.rb
@@ -16,11 +16,9 @@ RSpec.describe "Users", type: :request do
 		expect(parsed[:data][:id]).to eq(User.last.id.to_s)
 		expect(parsed[:data][:type]).to eq("user")
 		expect(parsed[:data][:attributes]).to be_a Hash
-		expect(parsed[:data][:attributes].keys).to match_array([:email, :api_key])
+		expect(parsed[:data][:attributes].keys).to match_array([:email])
 		expect(parsed[:data][:attributes][:email]).to be_a String
 		expect(parsed[:data][:attributes][:email]).to eq(User.last.email)
-		expect(parsed[:data][:attributes][:api_key]).to be_a String
-		expect(parsed[:data][:attributes][:api_key]).to eq(User.last.api_key)
 	end
 
 	it "will return an error if a proper email is not provided" do 
