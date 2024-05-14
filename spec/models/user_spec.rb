c1860bb8cff41a13d714ab88277ce11f6843dee1
Hoa Dam <hoangocdam@gmail.com>
2024-05-14 14:48:27 -0700

    feat:user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:habits).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('user@example.com').for(:email) }
+    it { should_not allow_value('userexample.com').for(:email) }
+  end
+end
