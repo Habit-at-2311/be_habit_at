ab6bb29b980c33929b29a3e94b3c90244966e1fc
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-15 04:57:06 -0600

    Update test after changing how user searched in DB

--- spec/requests/a_user_can_be_deleted_spec.rb
+++ spec/requests/a_user_can_be_deleted_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Users", type: :request do
 		expect(User.first.email).to eq("tigerwoods@gmail.com")
 
 		delete "/api/v0/users/:user_id", headers: { "Content-Type" => "application/json", "Accept" => "application/json" },
-			params: user.to_json
+			params: { id: user.id }.to_json
 
 		parsed = JSON.parse(response.body, symbolize_names: true)
 		
@@ -17,17 +17,17 @@ RSpec.describe "Users", type: :request do
 		expect(parsed).to eq({:message=> "User tigerwoods@gmail.com, was successfully deleted" })
 	end
 
-	it "will return an error if the user to be deleted can't be found" do 
+	it "will return an error if user ID not provided" do 
 		User.create!(email: "tigerwoods@gmail.com")
 
 		expect(User.count).to eq(1)
 		expect(User.first.email).to eq("tigerwoods@gmail.com")
 
-		delete "/api/v0/users/:user_id", headers: { "Content-Type" => "application/json", "Accept" => "application/json" },
-			params: { email: "wrong_email@yahoo.com" }.to_json
+		delete "/api/v0/users/:id", headers: { "Content-Type" => "application/json", "Accept" => "application/json" },
+			params: { id: 283734 }.to_json
 		
 		parsed = JSON.parse(response.body, symbolize_names: true)
-	
+		
 		expect(User.count).to eq(1)
 		expect(response.status).to eq(404)
 		expect(parsed).to eq({ :error=> "User not found" })
