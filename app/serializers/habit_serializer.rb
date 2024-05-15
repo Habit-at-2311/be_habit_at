fca731d9f28db636716fb2bc47e512f0ac454437
Hoa Dam <hoangocdam@gmail.com>
2024-05-15 00:42:35 -0700

    add:user_id to json

--- app/serializers/habit_serializer.rb
+++ app/serializers/habit_serializer.rb
@@ -1,4 +1,4 @@
 class HabitSerializer
   include JSONAPI::Serializer
-  attributes :name, :description, :frequency, :start_datetime, :end_datetime, :status
+  attributes :user_id, :name, :description, :frequency, :start_datetime, :end_datetime, :status
 end
