a70d8f4704b1a9f3d7af1ed4097a0bedad27063e
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-15 16:22:32 -0600

    Refactor error handling in users controller; move everything to application controller for better organization and global access scope

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,20 +1,17 @@
 class Api::V0::UsersController < ApplicationController
-	rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
-
 	def create
 		user = User.new(user_params)
-
 		if user.save
 			render json: UserSerializer.new(user), status: :ok
 		else
-			render json: user.errors.full_messages, status: :unprocessable_entity
+			raise ActiveRecord::RecordInvalid
 		end
 	end
 
 	def destroy
 		user = User.find(user_params[:id])
-
 		user.destroy
+
 		render json: { message: "User #{user.email}, was successfully deleted" }, status: :ok
 	end
 
@@ -27,11 +24,7 @@ class Api::V0::UsersController < ApplicationController
 
 	private
 
-	def handle_record_not_found
-		render json: { error: 'User not found' }, status: :not_found
-	end
-
 	def user_params
-		params.require(:user).permit(:id, :email)
+		params.require(:user).permit(:id, :name, :email)
 	end
 end
