738d498affd753b06b770807bf0a3da9783c39e6
Hoa Dam <hoangocdam@gmail.com>
2024-05-16 22:54:11 -0700

    feat:create_progresses of different frequencies

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,6 +2,7 @@ class Habit < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :frequency, presence: true
+  validates :custom_frequency, presence: true
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
   validates :status, presence: true
@@ -17,8 +18,42 @@ class Habit < ApplicationRecord
   private
 
   def create_progresses
+    if daily?
+      create_daily_progresses
+    elsif weekly?
+      create_weekly_progress
+    else
+      create_monthly_progress
+    end
+  end
+
+  def create_daily_progresses
     (start_datetime.to_date..end_datetime.to_date).each do |datetime|
       progresses.find_or_create_by(datetime: datetime)
     end
+  end
+
+  def create_weekly_progress
+    (start_datetime.to_date..end_datetime.to_date).each do |datetime|
+      day = datetime.strftime("%A")
+      if custom_frequency["#{day}"]
+        progresses.find_or_create_by(datetime: datetime)
+      end
+    end
+  end
+
+  def create_monthly_progress
+    (start_datetime.to_date..end_datetime.to_date).each do |datetime|
+      if first_week?(datetime)
+        day = datetime.strftime("%A")
+        if custom_frequency["#{day}"]
+          progresses.find_or_create_by(datetime: datetime)
+        end
+      end
+    end
+  end
+
+  def first_week?(date)
+    date.day <= 7
   end
 end
