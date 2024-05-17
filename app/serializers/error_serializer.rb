81320ee599eb12f24fe02e0ce074bc18ad2e2afd
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-17 15:03:05 -0600

    Add change to ErrorSerializer #serialize_error method; removes unneeded AR search query from original error message

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,13 +1,15 @@
 class ErrorSerializer
+  attr_reader :serialize_error
+
   def initialize(error)
     @error = error
     @status_code = error.status_code
   end
 
-  def serialize_json
+  def serialize_error
     {
       errors: [{
-        detail: @error.message,
+        detail: @error.message.scan(/^[^\[]+/).join.strip,
         status_code: @status_code
       }]
     }
