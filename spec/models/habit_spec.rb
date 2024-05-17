7fb7f26765408760e18d57ee2464dca014877944
Hoa Dam <hoangocdam@gmail.com>
2024-05-16 22:53:34 -0700

    add:test for creating progresses datas based on daily/weekly/ monthly frequency

--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Habit, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:frequency) }
+    it { should validate_presence_of(:custom_frequency) }
     it { should validate_presence_of(:start_datetime) }
     it { should validate_presence_of(:end_datetime) }
     it { should validate_presence_of(:status) }
@@ -16,9 +17,9 @@ RSpec.describe Habit, type: :model do
   end
 
   describe 'after_create_commit' do
-    it 'creates progresses for each day between start_datetime and end_datetime' do
+    it 'creates progresses for each day between start_datetime and end_datetime with frequency is daily' do
       user = User.create!({
-        name: "John Dunbar",
+        name: "John Doe",
         email: "john@gmail.com"
       })
       habit = Habit.create!({
@@ -26,6 +27,15 @@ RSpec.describe Habit, type: :model do
         description: "Spend 10 minutes meditating right after waking up",
         user_id: user.id,
         frequency: 0,
+        custom_frequency: {
+          'Monday': true,
+          'Tuesday': true,
+          'Wednesday': true,
+          'Thursday': true,
+          'Friday': true,
+          'Saturday': true,
+          'Sunday': true
+        },
         start_datetime: DateTime.new(2024,5,1),
         end_datetime: DateTime.new(2024,5,10),
         status: 0
@@ -33,6 +43,58 @@ RSpec.describe Habit, type: :model do
       expected_days = (DateTime.new(2024,5,1).to_date..DateTime.new(2024,5,10).to_date).count
 
       expect(habit.progresses.count).to eq(expected_days)
+    end
+
+    it 'creates progresses for each day between start_datetime and end_datetime with frequency is weekly' do
+      user = User.create!({
+        name: "John Doe",
+        email: "john@gmail.com"
+      })
+      habit = Habit.create!({
+        name: "Mediate",
+        description: "Spend 10 minutes meditating right after waking up",
+        user_id: user.id,
+        frequency: 1,
+        custom_frequency: {
+          'Monday': true,
+          'Tuesday': false,
+          'Wednesday': true,
+          'Thursday': false,
+          'Friday': true,
+          'Saturday': false,
+          'Sunday': false
+        },
+        start_datetime: DateTime.new(2024,5,1),
+        end_datetime: DateTime.new(2024,5,20),
+        status: 0
+      })
+      expect(habit.progresses.count).to eq(9)
+    end
+
+    it 'creates progresses for each day between start_datetime and end_datetime with frequency is monthly' do
+      user = User.create!({
+        name: "John Doe",
+        email: "john@gmail.com"
+      })
+      habit = Habit.create!({
+        name: "Mediate",
+        description: "Spend 10 minutes meditating right after waking up",
+        user_id: user.id,
+        frequency: 2,
+        custom_frequency: {
+          'Monday': true,
+          'Tuesday': false,
+          'Wednesday': false,
+          'Thursday': false,
+          'Friday': false,
+          'Saturday': false,
+          'Sunday': false
+        },
+        start_datetime: DateTime.new(2024,5,1),
+        end_datetime: DateTime.new(2024,12,1),
+        status: 0
+      })
+      expect(habit.progresses.count).to eq(7)
     end
   end
 end
