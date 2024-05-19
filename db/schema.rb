f1c55714076ad0316bca78222d91ec2bdb5ab9e8
Hoa Dam <hoangocdam@gmail.com>
2024-05-18 17:45:02 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_17_070540) do
 ActiveRecord::Schema[7.1].define(version: 2024_05_18_065717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
