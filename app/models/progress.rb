b07ef8c654d92d72fbf03a335ff52a6f6b9a8b8d
Hoa Dam <hoangocdam@gmail.com>
2024-06-04 15:39:32 -0700

    update habit_plant when the progress status is updated

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -5,4 +5,20 @@ class Progress < ApplicationRecord
   validates :datetime, presence: true
 
   enum status: [:incomplete, :completed, :skipped]
+
+  after_update_commit :update_plant
+
+  private
+
+  def update_plant
+    plant_growing =
+    case status
+    when 'completed'
+      false
+    when 'skipped'
+      true
+    end
+  grow_service = GrowPlantService.new(habit.habit_plant, plant_growing)
+  grow_service.update
+  end
 end
