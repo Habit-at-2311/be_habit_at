0170537ffa4057013713ff53faada4bf8452a1d3
Hoa Dam <hoangocdam@gmail.com>
2024-06-04 15:34:55 -0700

    add max_scale for plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,10 @@
 class Plant < ApplicationRecord
-	validates :style, :stem, :seed, :petal, :leaf, :scale, presence: true
+	validates :style, :stem, :seed, :petal, :leaf, :grow_rate, :scale, presence: true
 
-	has_many :habits, dependent: :destroy
+	has_many :habit_plants,  dependent: :destroy
+	has_many :habits, through: :habit_plants
+
+	def max_scale
+		4.0
+	end
 end
